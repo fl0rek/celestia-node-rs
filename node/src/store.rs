ce567201870c627d86ff6c2f773fe263633ab402
Mikołaj Florkiewicz <mikolaj@florkiewicz.me>
2024-04-04 09:45:14 +0200

    Add FifoStore

--- node/src/store.rs
+++ node/src/store.rs
@@ -7,12 +7,14 @@ use celestia_types::hash::Hash;
 use celestia_types::ExtendedHeader;
 use thiserror::Error;
 
+pub use fifo::FifoStore;
 pub use in_memory_store::InMemoryStore;
 #[cfg(target_arch = "wasm32")]
 pub use indexed_db_store::IndexedDbStore;
 #[cfg(not(target_arch = "wasm32"))]
 pub use sled_store::SledStore;
 
+mod fifo_store;
 mod in_memory_store;
 #[cfg(target_arch = "wasm32")]
 mod indexed_db_store;
